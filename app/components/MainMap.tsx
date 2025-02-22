9c414fb526f144b7ec214f7614282a6a7f66fbdd
ness727 <gnsdl727@naver.com>
2025-02-22 13:36:39 +0900

    100번 버스 실시간 위치 마커 레이어 표시 추가

--- app/components/MainMap.tsx
+++ app/components/MainMap.tsx
@@ -6,13 +6,21 @@ import { fromLonLat } from "ol/proj";
 import { OSM } from "ol/source";
 import { useEffect } from "react";
 import { Vector as VectorLayer } from "ol/layer";
-import { LineString } from "ol/geom";
+import { Vector as SourceVector } from "ol/source";
+import { LineString, Point } from "ol/geom";
 import VectorSource from "ol/source/Vector";
 import { Coordinate } from "ol/coordinate";
 import Style from "ol/style/Style";
 import Stroke from "ol/style/Stroke";
+import Icon from "ol/style/Icon";
 
-export default function MainMap({ numArr }: { numArr: number[][] }) {
+export default function MainMap({
+  busRoutePosArr,
+  busPosArr,
+}: {
+  busRoutePosArr: number[][];
+  busPosArr: number[][];
+}) {
   useEffect(() => {
     const map = new Map({
       target: "map",
@@ -28,21 +36,28 @@ export default function MainMap({ numArr }: { numArr: number[][] }) {
     });
 
     // 버스 노선 경로 벡터 레이어 추가
-    const vectorLayer = getVectorLayer(numArr);
-    map.addLayer(vectorLayer);
-  }, [numArr]);
+    const busRouteLayer = getBusRouteVectorLayer(busRoutePosArr);
+    map.addLayer(busRouteLayer);
+
+    // 버스 위치 마커 레이어 추가
+    busPosArr.map((busPos) => {
+      const busPosMarkerLayer = getBusPosMarkerLayer(busPos);
+      map.addLayer(busPosMarkerLayer);
+    });
+  }, [busRoutePosArr, busPosArr]);
 
   return <div id="map" style={{ width: "100vw", height: "100vh" }}></div>;
 }
 
-const getVectorLayer = (numArr: number[][]): VectorLayer => {
-  const result: Coordinate[] = [];
-  numArr.map((item) => {
-    result.push(fromLonLat([item[0], item[1]]));
+// 버스 노선 경로 벡터 레이어
+const getBusRouteVectorLayer = (busRoutePosArr: number[][]): VectorLayer => {
+  const coordinateArr: Coordinate[] = [];
+  busRoutePosArr.map((item) => {
+    coordinateArr.push(fromLonLat([item[0], item[1]]));
   });
 
   // LineString 생성
-  const lineString = new LineString(result);
+  const lineString = new LineString(coordinateArr);
 
   // Feature 생성
   const lineFeature = new Feature(lineString);
@@ -62,6 +77,40 @@ const getVectorLayer = (numArr: number[][]): VectorLayer => {
   // 벡터 레이어 생성
   const vectorLayer = new VectorLayer({
     source: vectorSource,
+  });
+
+  return vectorLayer;
+};
+
+// 버스 위치 마커 레이어
+const getBusPosMarkerLayer = (busPos: number[]): VectorLayer => {
+  // Point 생성
+  const point = new Point(fromLonLat([busPos[0], busPos[1]]));
+
+  // Feature 생성
+  const pointFeature = new Feature(point);
+
+  // 벡터 소스 및 레이어 생성
+  const vectorSource = new SourceVector({
+    features: [pointFeature],
+  });
+
+  // 마커 속성 지정
+  const markerStyle = new Style({
+    image: new Icon({
+      opacity: 1,
+      scale: 1.2,
+      src: "http://map.vworld.kr/images/ol3/marker_blue.png",
+    }),
+    zIndex: 100,
+  });
+
+  // vectorSource.getFeatures().map((feature) => feature.setStyle(markerStyle));
+
+  // 벡터 레이어 생성
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+    style: markerStyle,
   });
 
   return vectorLayer;
--- app/map/page.tsx
+++ app/map/page.tsx
@@ -19,9 +19,30 @@ interface ItemList {
   posY: string;
 }
 
-const URL = process.env.BUS_ROUTE_API;
-
+/* 버스 노선 경로 위치 가져오기
+  return [gpsX, gpsY] 2차원 배열
+*/
 async function getRoutePath() {
+  const URL = process.env.BUS_ROUTE_API;
+  const response = await fetch(URL || "");
+  const xml = await response.text();
+  const numArr: number[][] = [];
+
+  parseString(xml, (err, result) => {
+    const tempArr: ItemList[] = result.ServiceResult.msgBody[0].itemList;
+
+    tempArr.map((route: ItemList) =>
+      numArr.push([Number(route.gpsX), Number(route.gpsY)])
+    );
+  });
+  return numArr;
+}
+
+/* 버스 위치 가져오기
+  return [gpsX, gpsY] 2차원 배열
+*/
+async function getBusPosArr() {
+  const URL = process.env.BUS_POS_API;
   const response = await fetch(URL || "");
   const xml = await response.text();
   const numArr: number[][] = [];
@@ -37,12 +58,13 @@ async function getRoutePath() {
 }
 
 export default async function MapPage() {
-  const routeCoordinates: number[][] = await getRoutePath();
+  const routePosArr: number[][] = await getRoutePath();
+  const busPosArr: number[][] = await getBusPosArr();
 
   return (
     <div style={{ display: "flex", position: "relative" }}>
       <SideBar />
-      <MainMap numArr={routeCoordinates} />
+      <MainMap busRoutePosArr={routePosArr} busPosArr={busPosArr} />
     </div>
   );
 }
